420023f5e69993b4b862d65bd1eca9af8aa6256a
taemin kim <world32033@gmail.com>
2025-10-15 11:35:53 +0900

    feat: InputView 파일 생성 및 기본 구조 구현

---
+++ src/main/java/calculator/view/InputView.java
@@ -0,0 +1,10 @@
+package calculator.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public String intputString() {
+        System.out.println("덧셈할 문자열을 입력해주세요.");
+        return Console.readLine();
+    }
+}

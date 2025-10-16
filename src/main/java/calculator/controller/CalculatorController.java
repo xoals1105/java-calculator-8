cfa02c399059281befc4db5488986c2f93fd95b8
taemin kim <world32033@gmail.com>
2025-10-16 15:41:23 +0900

    feat: 커스텀 구분자 기능 구현

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -5,17 +5,15 @@ import calculator.view.InputView;
 import calculator.view.OutputView;
 
 public class CalculatorController {
-    private final InputView inputView = new InputView();
-    private final OutputView outputView = new OutputView();
+    public final InputView inputView = new InputView();
+    public final OutputView outputView = new OutputView();
     private final StringCalculator stringCalculator = new StringCalculator();
 
     public void runCalculation() {
-        try {
+
             String input = inputView.intputString();
             int result = stringCalculator.add(input);
             outputView.printResult(result);
-        } catch(IllegalArgumentException error) {
-            outputView.printError(error.getMessage());
-        }
+
     }
 }
--- src/main/java/calculator/model/StringCalculator.java
+++ src/main/java/calculator/model/StringCalculator.java
@@ -1,7 +1,12 @@
 package calculator.model;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
 
+    // 커스텀 구분자 패턴: //{구분자}\n{숫자}
+    private static final Pattern CUSTOM = Pattern.compile("//(.)\n(.*)");
     /**
      * 주어진 문자열의 숫자들을 더한 값을 반환한다.
      * - 빈/공백/NULL이면 0 반환
@@ -11,7 +16,39 @@ public class StringCalculator {
             return 0;
         }
 
-        // 기본 구문자/ 커스텀 구문자 로직 추후 추가
-        throw new IllegalArgumentException("");
+        text = text.replace("\\n", "\n");
+        String[] tokens = splitTokens(text);
+
+        int total = 0;
+        for(String token : tokens) {
+            int number = toInteger(token);
+            validateNonNegative(number);
+            total += number;
+        }
+        return total;
+    }
+
+    private String[] splitTokens(String text) {
+        Matcher matcher = CUSTOM.matcher(text);
+        if (matcher.matches()) {
+            String custom = Pattern.quote(matcher.group(1));
+            String numbers = matcher.group(2);
+            return numbers.split(custom);
+        }
+        return text.split(",|:");
+    }
+
+    private  int toInteger(String token) {
+        try {
+            return Integer.parseInt(token);
+        } catch (NumberFormatException error) {
+            throw new IllegalArgumentException("숫자가 아닌 값이 포함됐습니다.");
+        }
+    }
+
+    private void validateNonNegative(int number) {
+        if(number < 0) {
+            throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
+        }
     }
 }
